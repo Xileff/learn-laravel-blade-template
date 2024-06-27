12f3e4e08f754e87da6bb2f7f9b44ca9d5d227c2
Felix <felixsavero78@gmail.com>
2024-06-27 11:57:13 +0700

    feat: unless statement

---
+++ resources/views/unless.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Unless</title>
+</head>
+
+<body>
+    @unless ($isAdmin)
+        You are not an Admin.
+    @endunless
+</body>
+
+</html>
---
+++ tests/Feature/UnlessTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UnlessTest extends TestCase
+{
+    public function testUnless()
+    {
+        $this->view('unless', ['isAdmin' => true])->assertDontSeeText('You are not an Admin.');
+        $this->view('unless', ['isAdmin' => false])->assertSeeText('You are not an Admin.');
+    }
+}
