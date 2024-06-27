e6e5a9230a9f997ce2e01eff76b61673195001c2
Felix <felixsavero78@gmail.com>
2024-06-27 11:45:45 +0700

    feat: disable blade template

---
+++ resources/views/disabled.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <h1>Hello @{{ $name }}</h1>
+
+    @verbatim
+        Hello {{ $name }}
+        Hello {{ $name }}
+    @endverbatim
+</body>
+
+</html>
